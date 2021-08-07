747a9dcfe63fbff7421dd2302fa751aa669c5284
sarahclarke-skc <sarahkathrynclarke@gmail.com>
2021-08-07 17:46:05 +0100

    working on the controller

--- controllers/controller.py
+++ controllers/controller.py
@@ -7,5 +7,12 @@ from models.player import *
 def index():
     return render_template('base.html', title="Rock Paper Scissors")
 
+@app.route('/<choice1>/<choice2>')
+def play_game():
+    player1 = Player("player1", "choice1")
+    player2 = Player("player2", "choice2")
+    Game.get_winner(player1, player2)
+    return render_template('index.html', title="game")
+
     # return game.get_winner(game.player1.choice, game.player2.choice)
 #return a string
