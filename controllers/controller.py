508037b6ee51b08b19fc00786d54f008462e4555
sarahclarke-skc <sarahkathrynclarke@gmail.com>
2021-08-07 18:50:07 +0100

    Web browser game working

--- controllers/controller.py
+++ controllers/controller.py
@@ -8,11 +8,15 @@ def index():
     return render_template('base.html', title="Rock Paper Scissors")
 
 @app.route('/<choice1>/<choice2>')
-def play_game():
-    player1 = Player("player1", "choice1")
-    player2 = Player("player2", "choice2")
-    Game.get_winner(player1, player2)
-    return render_template('index.html', title="game")
+def play_game(choice1, choice2):
+    player1 = Player("player 1", choice1)
+    player2 = Player("player 2", choice2)
+    game = Game(player1,player2)
+    winner = game.get_winner(player1, player2)
+    return render_template('index.html', title="Results", winner = winner, choice1 = choice1, choice2 = choice2)
+
+
+
 
     # return game.get_winner(game.player1.choice, game.player2.choice)
 #return a string
--- models/game.py
+++ models/game.py
@@ -1,11 +1,12 @@
 from models.player import Player
 
 class Game():
-    def __init__(self, player):
+    def __init__(self, player, player2):
         self.player = player
+        self.player2 = player2
     
-    player1 = Player("John", "rock")
-    player2 = Player("Sam", "paper")
+    player1 = Player("player 1", "rock")
+    player2 = Player("player 2", "paper")
 
     def get_winner(self, player1, player2):
         if player1.choice == "rock" and player2.choice == "paper":
@@ -20,6 +21,10 @@ class Game():
             return f"Congratulations {player2.name}, you have won!"
         elif player1.choice == "rock" and player2.choice == "paper":
             return f"Congratulations {player2.name}, you have won!"
+        elif player1.choice == "paper" and player2.choice == "scissors":
+            return f"Congratulations {player2.name}, you have won!"
+        elif player1.choice == "scissors" and player2.choice == "paper":
+            return f"Congratulations {player1.name}, you have won!"
         
 
         
